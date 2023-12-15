b8c85ba929442583c136e37d0bec858e8d32ea72
carolangel19 <102700849+CarolAngel19@users.noreply.github.com>
2023-12-15 12:30:36 -0500

    Codelab: Unit Testing

--- src/main/java/org/adaschool/Weather/service/WeatherReportService.java
+++ src/main/java/org/adaschool/Weather/service/WeatherReportService.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class WeatherReportService {
 
-    private static final String API_KEY = "your-api-key";
+    private static final String API_KEY = "2b1c50ccf5ce77f697b21ce34b1cf76a";
     private static final String API_URL = "https://api.openweathermap.org/data/2.5/weather";
 
     public WeatherReport getWeatherReport(double latitude, double longitude) {
--- src/test/java/org/adaschool/Weather/WeatherApplicationTests.java
+++ src/test/java/org/adaschool/Weather/WeatherApplicationTests.java
@@ -3,11 +3,49 @@ package org.adaschool.Weather;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+import org.adaschool.Weather.controller.WeatherReportController;
+import org.adaschool.Weather.data.WeatherReport;
+import org.adaschool.Weather.service.WeatherReportService;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.Mockito.when;
+
 @SpringBootTest
 class WeatherApplicationTests {
 
+	@Mock
+	private WeatherReportService weatherReportService;
+
+	private WeatherReportController weatherReportController;
+
+	@BeforeEach
+	void setUp() {
+		weatherReportController = new WeatherReportController(weatherReportService);
+	}
+
 	@Test
-	void contextLoads() {
+	void testReturnsCorrectWeatherReport() {
+		double latitude = 37.8267;
+		double longitude = -122.4233;
+
+		// Un objeto WeatherReport que devuelva el servicio
+		WeatherReport expectedWeatherReport = new WeatherReport();
+		expectedWeatherReport.setTemperature(25.0);
+		expectedWeatherReport.setHumidity(70.0);
+
+		when(weatherReportService.getWeatherReport(anyDouble(), anyDouble()))
+				.thenReturn(expectedWeatherReport);
+
+		WeatherReport actualWeatherReport = weatherReportController.getWeatherReport(latitude, longitude);
+
+		// Verificar que el resultado del controlador sea igual al objeto esperado
+		assertEquals(expectedWeatherReport, actualWeatherReport);
 	}
 
 }
+
